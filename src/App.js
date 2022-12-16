07c26eadef0604899c85f3f08bcf8d6f55e080fe
Kae Sluder <kaesluder@fastmail.com>
2022-12-16 13:30:29 -0500

    chat entry test, passes wave 1 tests

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import './App.css';
+import ChatEntry from './components/ChatEntry';
 import chatMessages from './data/messages.json';
 
 const App = () => {
+  const firstMessage = chatMessages[0];
+  console.log(firstMessage);
   return (
     <div id="App">
       <header>
         <h1>Application title</h1>
       </header>
       <main>
-        {/* Wave 01: Render one ChatEntry component
+        <ChatEntry
+          id={firstMessage.id}
+          sender={firstMessage.sender}
+          body={firstMessage.body}
+          timeStamp={firstMessage.timeStamp}
+        ></ChatEntry>
+        {/* Wave  01: Render one ChatEntry component
         Wave 02: Render ChatLog component */}
       </main>
     </div>
